1e5d2c821607a582b31f47ebf2c7d7d09739ef93
Ridit Jangra <riditjangra09@gmail.com>
2026-03-27 20:03:15 +0530

    chore: update InputBox, ChatView, ToolCall

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -4,4 +4,3 @@ export { CommandPalette } from "./components/CommandPalette";
 export { TextArea } from "./components/TextArea";
 export { InputBox, ShortcutBar } from "./components/InputBox";
 export { MessageBody } from "./components/MessageBody";
-// remove old Input, CommandPalette, Markdown exports or keep if still used
