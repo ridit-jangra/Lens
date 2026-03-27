7571a126b960d7d87163f624cd27343fe5f31419
Ridit Jangra <riditjangra09@gmail.com>
2026-03-27 16:25:05 +0530

    feat: export all components from @ridit/ink-ui

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,1 +1,4 @@
 export { Markdown } from "./components/Markdown";
+export { Diff } from "./components/Diff";
+export { Input } from "./components/Input";
+export { CommandPalette } from "./components/CommandPalette";
