a5a4e00edb531382c32b09283a1a4f7f36f11381
Ridit Jangra <riditjangra09@gmail.com>
2026-03-27 18:31:39 +0530

    feat: export all modules from @ridit/lens-core

---
+++ packages/core/src/index.ts
@@ -0,0 +1,34 @@
+// agent
+export { chat } from "./agent";
+
+// session
+export { createSession, addMessage, getMessages } from "./session";
+export type { Session } from "./session";
+
+// memory
+export {
+  getSystemPrompt,
+  loadSession,
+  saveSession,
+  loadGlobalMemory,
+  saveGlobalMemory,
+  getRepoId,
+  sessionExists,
+} from "./memory";
+
+// config
+export {
+  loadConfig,
+  saveConfig,
+  configExists,
+  getActiveProvider,
+  setActiveProvider,
+  addProvider,
+} from "./config";
+export type { Config, Provider, ProviderSettings } from "./config";
+
+// tools
+export { tools, read, write, bash, grep, ls, remember } from "./tools";
+
+// providers
+export { createProvider } from "./providers";
